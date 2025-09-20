59ef987c8633424451bd67fa8378f2ba732299bd
Bhavin sorathiya <142117033+bhavinoo7@users.noreply.github.com>
2025-09-20 19:03:06 +0530

    Update resume-preview.tsx

--- components/resume-preview.tsx
+++ components/resume-preview.tsx
@@ -11,7 +11,7 @@ export function ResumePreview() {
     const fileId = "YOUR_GOOGLE_DRIVE_FILE_ID"
 
     // Create the direct download link
-    const downloadUrl = `https://drive.google.com/file/d/1GbeTAe-glQgPNs2SHjhBMGQFGIAlbzff/view?usp=sharing`
+    const downloadUrl = `https://drive.google.com/file/d/1xXWWEpbCp94i8XXsUPjueLoECczVaeP8/view?usp=sharing`
 
     // Open the download URL in a new tab
     window.open(downloadUrl, "_blank")
@@ -25,7 +25,7 @@ export function ResumePreview() {
             <div className="relative mb-4 w-full overflow-hidden rounded-md border border-border bg-white shadow-sm dark:bg-gray-900">
               {/* Resume preview image */}
               <img
-                src="https://res.cloudinary.com/durtlcmnb/image/upload/v1756876136/airbnb_DEV/Screenshot_2025-09-03_103604_etcxwk.png"
+                src="https://res.cloudinary.com/durtlcmnb/image/upload/v1758374874/Screenshot_2025-09-20_185349_hppjpz.png"
                 alt="Resume Preview"
                 className="h-auto w-full object-cover"
               />
